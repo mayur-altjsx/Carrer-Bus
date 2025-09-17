be751a97235f60dbb4fab013a99722002b2aecd5
MayurK-cmd <mayurgk2006@gmail.com>
2025-09-17 18:11:21 +0530

    routing

--- frontend/src/pages/Colleges.jsx
+++ frontend/src/pages/Colleges.jsx
@@ -28,7 +28,7 @@ function Colleges() {
   };
 
   const token = localStorage.getItem("token");
-  const API_BASE = "https://college-data-glhv.onrender.com";
+  const API_BASE = "https://sih-73ja.onrender.com/api";
 
   const clearInputs = () => {
     setType("");
